bc8572038fd9105e976be45fa312f6c445f62251
Revision <tkn963@gmail.com>
2021-12-09 16:08:13 +0100

    Area
    Removed the unused column.

--- sql/world/base/mod_spawnpoints.sql
+++ sql/world/base/mod_spawnpoints.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS `mod_spawnpoints`;
 CREATE TABLE `mod_spawnpoints` (
     `team_id` INT NOT NULL,
     `map_id` INT NOT NULL,
-    `area_id` INT NOT NULL,
     `pos_x` FLOAT NOT NULL,
     `pos_y` FLOAT NOT NULL,
     `pos_z` FLOAT NOT NULL,
@@ -13,5 +12,5 @@ CREATE TABLE `mod_spawnpoints` (
 COMMENT='Spawn points'
 COLLATE='utf8mb4_general_ci';
 
-INSERT INTO `mod_spawnpoints` (`team_id`, `map_id`, `area_id`, `pos_x`, `pos_y`, `pos_z`, `orientation`, `comment`) VALUES (0, 0, 1519, -8830.44, 626.666, 93.9829, 0.682076, 'Alliance - Stormwind City');
-INSERT INTO `mod_spawnpoints` (`team_id`, `map_id`, `area_id`, `pos_x`, `pos_y`, `pos_z`, `orientation`, `comment`) VALUES (1, 1, 1637, 1630.78, -4412.99, 16.5677, 0.080535, 'Horde - Orgrimmar');
+INSERT INTO `mod_spawnpoints` (`team_id`, `map_id`, `pos_x`, `pos_y`, `pos_z`, `orientation`, `comment`) VALUES (0, 0, -8830.44, 626.666, 93.9829, 0.682076, 'Alliance - Stormwind City');
+INSERT INTO `mod_spawnpoints` (`team_id`, `map_id`, `pos_x`, `pos_y`, `pos_z`, `orientation`, `comment`) VALUES (1, 1, 1630.78, -4412.99, 16.5677, 0.080535, 'Horde - Orgrimmar');
