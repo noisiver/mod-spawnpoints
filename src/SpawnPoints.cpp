84ef386a15fcdc9b7057a87b9972ecb260e05d77
Revision <tkn963@gmail.com>
2021-12-10 17:47:07 +0100

    Log
    I'm testing this to see if it'll make a blank space between module logs.

--- src/SpawnPoints.cpp
+++ src/SpawnPoints.cpp
@@ -21,31 +21,33 @@ class SpawnPointData : public WorldScript
         void OnStartup() override
         {
             LOG_INFO("server.loading", "Loading spawn points...");
-            QueryResult result = WorldDatabase.Query("SELECT `team_id`, `map_id`, `pos_x`, `pos_y`, `pos_z`, `orientation` FROM `mod_spawnpoints`");
-
-            if (!result)
             {
-                LOG_INFO("server.loading", ">> Loaded 0 spawn points. DB table `mod_spawnpoints` is empty.");
-                return;
+                QueryResult result = WorldDatabase.Query("SELECT `team_id`, `map_id`, `pos_x`, `pos_y`, `pos_z`, `orientation` FROM `mod_spawnpoints`");
+
+                if (!result)
+                {
+                    LOG_INFO("server.loading", ">> Loaded 0 spawn points. DB table `mod_spawnpoints` is empty.");
+                    return;
+                }
+
+                int i = 0;
+                do
+                {
+                    Field* fields = result->Fetch();
+                    uint32 TeamId = fields[0].GetUInt32();
+
+                    spawnPoints.push_back(SpawnPoints());
+                    spawnPoints[TeamId].MapId = fields[1].GetUInt32();
+                    spawnPoints[TeamId].X = fields[2].GetFloat();
+                    spawnPoints[TeamId].Y = fields[3].GetFloat();
+                    spawnPoints[TeamId].Z = fields[4].GetFloat();
+                    spawnPoints[TeamId].O = fields[5].GetFloat();
+
+                    i++;
+                } while (result->NextRow());
+
+                LOG_INFO("server.loading", ">> Loaded %i spawn points", i);
             }
-
-            int i = 0;
-            do
-            {
-                Field* fields = result->Fetch();
-                uint32 TeamId = fields[0].GetUInt32();
-
-                spawnPoints.push_back(SpawnPoints());
-                spawnPoints[TeamId].MapId = fields[1].GetUInt32();
-                spawnPoints[TeamId].X = fields[2].GetFloat();
-                spawnPoints[TeamId].Y = fields[3].GetFloat();
-                spawnPoints[TeamId].Z = fields[4].GetFloat();
-                spawnPoints[TeamId].O = fields[5].GetFloat();
-
-                i++;
-            } while (result->NextRow());
-
-            LOG_INFO("server.loading", ">> Loaded %i spawn points", i);
         }
 };
 
