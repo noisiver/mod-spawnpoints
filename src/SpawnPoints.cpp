1a525b5b8889fcd48ae937b3d380ce4497c8211a
Revision <tkn963@gmail.com>
2022-06-06 01:58:29 +0200

    Standard
    Updated the code standard to match AzerothCore.

--- src/SpawnPoints.cpp
+++ src/SpawnPoints.cpp
@@ -15,64 +15,64 @@ std::vector<SpawnPoints> spawnPoints;
 
 class SpawnPointsPlayer : public PlayerScript
 {
-    public:
-        SpawnPointsPlayer() : PlayerScript("SpawnPointsPlayer") {}
+public:
+    SpawnPointsPlayer() : PlayerScript("SpawnPointsPlayer") {}
 
-        void OnCreate(Player* player) override
-        {
-            if (player->getClass() == CLASS_DEATH_KNIGHT)
-                return;
+    void OnCreate(Player* player) override
+    {
+        if (player->getClass() == CLASS_DEATH_KNIGHT)
+            return;
 
-            uint32 mapId                  = spawnPoints[player->GetTeamId()].MapId;
-            float x                       = spawnPoints[player->GetTeamId()].X;
-            float y                       = spawnPoints[player->GetTeamId()].Y;
-            float z                       = spawnPoints[player->GetTeamId()].Z;
-            float orientation             = spawnPoints[player->GetTeamId()].O;
-            const WorldLocation &location = WorldLocation(mapId, x, y, z, orientation);
+        uint32 mapId = spawnPoints[player->GetTeamId()].MapId;
+        float x = spawnPoints[player->GetTeamId()].X;
+        float y = spawnPoints[player->GetTeamId()].Y;
+        float z = spawnPoints[player->GetTeamId()].Z;
+        float orientation = spawnPoints[player->GetTeamId()].O;
+        const WorldLocation& location = WorldLocation(mapId, x, y, z, orientation);
 
-            player->Relocate(&location);
-            player->ResetMap();
-            player->SetMap(sMapMgr->CreateMap(mapId, player));
-            player->SaveToDB(false, false);
-        }
+        player->Relocate(&location);
+        player->ResetMap();
+        player->SetMap(sMapMgr->CreateMap(mapId, player));
+        player->SaveToDB(false, false);
+    }
 };
 
 class SpawnPointWorld : public WorldScript
 {
-    public:
-        SpawnPointWorld() : WorldScript("SpawnPointWorld") { }
+public:
+    SpawnPointWorld() : WorldScript("SpawnPointWorld") { }
 
-        void OnStartup() override
+    void OnStartup() override
+    {
+        LOG_INFO("server.loading", "Loading spawn points...");
         {
-            LOG_INFO("server.loading", "Loading spawn points...");
+            QueryResult result = WorldDatabase.Query("SELECT `team_id`, `map_id`, `pos_x`, `pos_y`, `pos_z`, `orientation` FROM `mod_spawnpoints`");
+
+            if (!result)
             {
-                QueryResult result = WorldDatabase.Query("SELECT `team_id`, `map_id`, `pos_x`, `pos_y`, `pos_z`, `orientation` FROM `mod_spawnpoints`");
-
-                if (!result)
-                {
-                    LOG_INFO("server.loading", ">> Loaded 0 spawn points. DB table `mod_spawnpoints` is empty.");
-                    return;
-                }
-
-                int i = 0;
-                do
-                {
-                    Field* fields = result->Fetch();
-                    uint32 TeamId = fields[0].Get<uint32>();
-
-                    spawnPoints.push_back(SpawnPoints());
-                    spawnPoints[TeamId].MapId = fields[1].Get<uint32>();
-                    spawnPoints[TeamId].X     = fields[2].Get<float>();
-                    spawnPoints[TeamId].Y     = fields[3].Get<float>();
-                    spawnPoints[TeamId].Z     = fields[4].Get<float>();
-                    spawnPoints[TeamId].O     = fields[5].Get<float>();
-
-                    i++;
-                } while (result->NextRow());
-
-                LOG_INFO("server.loading", ">> Loaded {} spawn points", i);
+                LOG_INFO("server.loading", ">> Loaded 0 spawn points. DB table `mod_spawnpoints` is empty.");
+                return;
             }
+
+            int i = 0;
+            do
+            {
+                Field* fields = result->Fetch();
+                uint32 TeamId = fields[0].Get<uint32>();
+
+                spawnPoints.push_back(SpawnPoints());
+                spawnPoints[TeamId].MapId = fields[1].Get<uint32>();
+                spawnPoints[TeamId].X = fields[2].Get<float>();
+                spawnPoints[TeamId].Y = fields[3].Get<float>();
+                spawnPoints[TeamId].Z = fields[4].Get<float>();
+                spawnPoints[TeamId].O = fields[5].Get<float>();
+
+                i++;
+            } while (result->NextRow());
+
+            LOG_INFO("server.loading", ">> Loaded {} spawn points", i);
         }
+    }
 };
 
 void AddSpawnPointsScripts()
